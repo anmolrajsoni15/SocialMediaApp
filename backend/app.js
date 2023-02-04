38346602a7188ecc9664cb85e25b7a85705fe979
Anmol Raj Soni <anmolrajsoni15@gmail.com>
2023-02-04 14:18:29 +0530

    Ecommerce website 1

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,6 @@ const path = require("path");
 if (process.env.NODE_ENV !== "production") {
     require("dotenv").config({ path: "backend/config/config.env" });
 }
-
 //using middlewares
 app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({limit: "50mb", extended: true}));
